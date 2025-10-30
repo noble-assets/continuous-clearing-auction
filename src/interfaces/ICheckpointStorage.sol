08849fb66e451aa2715a64a1ff366c3bf080bf07
Eric Zhong <eric.zhong@uniswap.org>
2025-10-30 16:47:43 -0400

    CVF-119: update interface for ICheckpointStorage

--- src/interfaces/ICheckpointStorage.sol
+++ src/interfaces/ICheckpointStorage.sol
@@ -6,14 +6,20 @@ import {Checkpoint} from '../libraries/CheckpointLib.sol';
 /// @notice Interface for checkpoint storage operations
 interface ICheckpointStorage {
     /// @notice Get the latest checkpoint at the last checkpointed block
+    /// @dev Be aware that the latest checkpoint may not be up to date, it is recommended
+    ///      to always call `checkpoint()` before using getter functions
     /// @return The latest checkpoint
     function latestCheckpoint() external view returns (Checkpoint memory);
 
     /// @notice Get the clearing price at the last checkpointed block
-    /// @return The current clearing price
+    /// @dev Be aware that the latest checkpoint may not be up to date, it is recommended
+    ///      to always call `checkpoint()` before using getter functions
+    /// @return The current clearing price in Q96 form
     function clearingPrice() external view returns (uint256);
 
     /// @notice Get the number of the last checkpointed block
+    /// @dev Be aware that the last checkpointed block may not be up to date, it is recommended
+    ///      to always call `checkpoint()` before using getter functions
     /// @return The block number of the last checkpoint
     function lastCheckpointedBlock() external view returns (uint64);
 
