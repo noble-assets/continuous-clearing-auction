8267974450b45a13c31ab471e06874049a671d8c
keyleu <kleuyckx@gmail.com>
2026-01-26 14:36:22 +0000

    make AuctionNoble ERC20 not ERC20Burnable

--- script/test/AuctionNoble.s.sol
+++ script/test/AuctionNoble.s.sol
@@ -4,8 +4,8 @@ pragma solidity 0.8.26;
 import {ContinuousClearingAuction} from '../../src/ContinuousClearingAuction.sol';
 import {AuctionParameters} from '../../src/interfaces/IContinuousClearingAuction.sol';
 import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
+import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import {IERC20} from '@openzeppelin/contracts/token/ERC20/IERC20.sol';
-import {ERC20, ERC20Burnable} from '@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol';
 import {SafeERC20} from '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
 import {Script} from 'forge-std/Script.sol';
 import {console} from 'forge-std/console.sol';
@@ -30,7 +30,7 @@ contract NobleBurner is INobleBurner {
     }
 }
 
-contract AuctionNoble is ERC20Burnable, Ownable {
+contract AuctionNoble is ERC20, Ownable {
     using SafeERC20 for IERC20;
 
     address immutable NOBLE = 0xe995e5A3A4BF15498246D7620CA39f7409397326;
