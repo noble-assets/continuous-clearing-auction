853aeb5cdb1ceefd2b4f342fd4cb008279993f4e
drewcook <drew@dco.dev>
2026-02-19 11:50:38 -0700

    feat: update to auction off 1M supply

--- script/deploy/DeployNobleAuctionWithSteps.s.sol
+++ script/deploy/DeployNobleAuctionWithSteps.s.sol
@@ -123,7 +123,7 @@ contract DeployNobleAuctionWithStepsScript is Script {
     }
 
     // Token amounts
-    uint128 constant AUCTION_SUPPLY = 10_000_000e18; // 10M NOBLE
+    uint128 constant AUCTION_SUPPLY = 1_000_000e18; // 1M NOBLE
     uint256 constant DEPLOYER_NOBLE = 1e18; // 1 NOBLE
     uint256 constant DEPLOYER_USDC = 1_000_000e6; // 1M USDC
 
